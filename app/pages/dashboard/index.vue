99a59615ee609ecd2f8ebf611b0236aa32309093
Matheus Augusto <auguzsto@gmail.com>
2025-04-22 23:48:41 -0300

    feat: Dashboard para retorno de chave de transmissão.

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,5 +1,23 @@
 <template>
-    <h1>
-        Você está autenticado e autorizado.
-    </h1>
+    <div class="h-full flex justify-center items-center">
+        <div v-if="error">
+            {{ error.data.message }}
+        </div>
+        <div v-if="data">
+            
+            <div class="w-4xl">
+                <b>Chave para transmissão</b>
+                <p class="break-all">{{ data?.key }}</p>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script setup lang="ts">
+const { data, error } = await useFetch("/api/stream/key", {
+    headers: {
+        "Authorization": `Bearer ${useCookie("Authorization").value}`
+    }
+})
+
+</script>
