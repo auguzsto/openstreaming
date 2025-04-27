d4d7f3f3da7d659da389148218c4323aa7844a8d
Matheus Augusto <auguzsto@gmail.com>
2025-04-27 19:35:28 -0300

    fix: Autenticação e autorização em signin store.

--- app/store/signin.ts
+++ app/store/signin.ts
@@ -1,4 +1,6 @@
 import type { FormSubmitEvent } from "@nuxt/ui";
+import { useUserStore } from "./user";
+import type { User } from "~/src/users/User";
 
 interface SignInInterfaceStore {
     isLoading: boolean,
@@ -17,20 +19,36 @@ export const useSignInStore = defineStore("signin", {
 
     actions: {
         async onSubmit(body: object) {
-            console.log(body)
+            const userStore = useUserStore()
             this.isLoading = true;
-            const { error, status, data } = await useFetch("/api/public/auth/signin", {
+            const { status: statusSingIn, error: errorSignIn } = await useFetch("/api/public/auth/signin", {
                 method: "POST",
                 body: (body as FormSubmitEvent<object>).data,
                 watch: false
             })
 
-            if (error) {
+            if (statusSingIn.value != "success") {
                 this.error = true,
-                this.message = error.value?.data.message
+                this.message = errorSignIn.value?.data.message
+                this.isLoading = false;
+                return;
             }
 
-            this.isLoading = false;
+            const { status: statusMe, error: errorMe, data } = await useFetch("/api/auth/me", {
+                headers: {
+                    "Authorization": `Bearer ${useCookie("Authorization").value}`
+                }
+            })
+
+            if (statusMe.value != "success") {
+                this.error = true,
+                this.message = errorMe.value?.data.message
+                this.isLoading = false;
+                return;
+            }
+
+            userStore.setUser(data.value as unknown as User)
+            navigateTo("/dashboard")
         }
     }
 })
