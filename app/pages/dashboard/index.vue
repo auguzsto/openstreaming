fd0dbcc4dd380508e0e4c189c34974d08a5a92b1
Matheus Augusto <auguzsto@gmail.com>
2025-04-27 21:23:46 -0300

    fix: Adição de instrução e servidor em tela de dashboard.

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -7,6 +7,14 @@
             <div class="w-4xl">
                 <b>Chave para transmissão</b>
                 <p class="break-all">{{ data?.key }}</p>
+
+                <hr class="m-2">
+                <b>Servidor</b>
+                <p class="break-all">rtmp://127.0.0.1:1935/live</p>
+                <hr class="m-2">
+
+                <b>Instrução</b>
+                <p class="break-all">Em seu aplicativo de transmissão (OBS Studio ou Streamlabs, por exemplo), selecione transmissão personalizada e preencha os dados solicitados como: servidor e chave transmissão.</p>
             </div>
         </div>
     </div>
